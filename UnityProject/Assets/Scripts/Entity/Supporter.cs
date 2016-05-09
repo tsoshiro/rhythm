88c01dd4e442a259b385ade01d2da3d912f334e3
toshiro <tsubasa.oshiro@gmail.com>
2016-05-10 01:54:01 +0900

    Supporterクラス初期化処理

--- UnityProject/Assets/Scripts/Entity/Supporter.cs
+++ UnityProject/Assets/Scripts/Entity/Supporter.cs
@@ -1,12 +1,32 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class Supporter{
+public class Supporter {
+	public int id; // サポーターID
+	public string name; // サポーター名
 	public int level; //現在のレベル
 	public int pointPerSecond; // 現在の1秒あたりポイント
 	public int nextLevelCoin; // 次のレベルに必要なコイン
 
+
 	public void levelUp(int pAddLevel) {
 		level += pAddLevel;
+	}
+
+	public Supporter(int pId, int pLevel) {
+		id = pId;
+		name = getSupporterName (pId);
+		level = pLevel;
+		pointPerSecond = getSupporterPps (pId, pLevel);
+	}
+
+	string getSupporterName(int pId) {
+		string result = "";
+		return result;
+	}
+
+	int getSupporterPps(int pId, int pLevel) {
+		int value = 0;
+		return value;
 	}
 }
