54a15e94e15fb007105769384eb78fa405cf100e
toshiro <tsubasa.oshiro@gmail.com>
2016-05-09 23:58:38 +0900

    サポータークラス実装開始。

--- UnityProject/Assets/Scripts/Entity/Supporter.cs
+++ UnityProject/Assets/Scripts/Entity/Supporter.cs
@@ -2,5 +2,11 @@
 using System.Collections;
 
 public class Supporter{
+	public int level; //現在のレベル
+	public int pointPerSecond; // 現在の1秒あたりポイント
+	public int nextLevelCoin; // 次のレベルに必要なコイン
 
+	public void levelUp(int pAddLevel) {
+		level += pAddLevel;
+	}
 }
