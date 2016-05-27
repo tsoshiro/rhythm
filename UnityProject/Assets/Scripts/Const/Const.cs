3a5f6270de6436f28003ad6d83ca5ca3dbabf64e
Tsubasa Oshiro <tsubasa.oshiro@gmail.com>
2016-05-27 23:58:49 +0900

    Audio系のConst変数を作成。

--- UnityProject/Assets/Scripts/Const/Const.cs
+++ UnityProject/Assets/Scripts/Const/Const.cs
@@ -7,4 +7,13 @@ public class Const {
 	public const string PREF_USER_MAX_COMBO = "MaxCombo";
 	public const string PREF_KILL_COUNT = "KillCount";
 	public const string PREF_ENMEY_NUM = "EnemyNum";
+
+	public const int SE_KICK 			= 0;
+	public const int SE_HAT_BAD 		= 1;
+	public const int SE_HAT_GOOD		= 2;
+	public const int SE_HAT_GREAT 		= 3;
+	public const int SE_HAT_EXCELLENT 	= 4;
+	public const int SE_SNARE 			= 5;
+	public const int SE_LV_UP 			= 6;
+	public const int SE_KILL_ENEMY 		= 7;
 }
